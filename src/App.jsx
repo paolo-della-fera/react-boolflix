cddfdd58701aa3aa93b6f2c7a8b7b093afbeab47
paolo-della-fera <paolodf2005@gmail.com>
2026-04-27 17:21:27 +0200

    refactor API key usage and clean up code

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import AppHeader from "./components/AppHeader"
 import AppMain from "./components/AppMain"
 import { useState } from "react"
-
 import axios from "axios"
 
-const api_env = import.meta.env.VITE_API_KEY
+
+const API_KEY = import.meta.env.VITE_API_KEY
 
 
 function App() {
-
   const [searchFilm, setSearchFilm] = useState('')
   const [film, setFilm] = useState([])
-
-  const api_url_movies = `https://api.themoviedb.org/3/search/movie?api_key=${api_env}&query=${searchFilm}`
-  const api_url_serie_tv = `https://api.themoviedb.org/3/search/tv?api_key=${api_env}&query=${searchFilm}`
 
 
   function handleSubmit(e) {
     e.preventDefault()
 
-    // chiamate API  
+    
+    const api_url_movies = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchFilm}`
+    const api_url_serie_tv = `https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&query=${searchFilm}`
+
+
     axios.get(api_url_movies)
       .then(res1 => {
         axios.get(api_url_serie_tv)
@@ -27,9 +27,8 @@ function App() {
             setFilm([...res1.data.results, ...res2.data.results])
           })
       })
-    console.log("KEY:", process.env.REACT_APP_API_KEY);
+      
   }
-
 
   return (
     <>
